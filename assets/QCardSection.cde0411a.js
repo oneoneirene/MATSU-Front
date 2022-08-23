08af75f76fa34063be816441b8f8b4f8596d3dc8
oneoneirene <101792723+oneoneirene@users.noreply.github.com>
2022-08-23 03:45:40 +0000

    Deploying to gh-pages from @ oneoneirene/MATSU-Front@3af392e85948e2b6258dda0e997acc7a1bb8e220 🚀

---
+++ assets/QCardSection.cde0411a.js
@@ -0,0 +1,1 @@
+import{m as e,n as r,q as n,s}from"./index.46c58dce.js";var i=e({name:"QCardSection",props:{tag:{type:String,default:"div"},horizontal:Boolean},setup(a,{slots:t}){const o=r(()=>`q-card__section q-card__section--${a.horizontal===!0?"horiz row no-wrap":"vert"}`);return()=>n(a.tag,{class:o.value},s(t.default))}});export{i as Q};
